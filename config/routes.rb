0617241bccedc07fe464cc44f4a7157024199a89
Ji Lee <gafrong@gmail.com>
2015-07-18 18:50:44 +1000

    removed urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :orders
+  # resources :orders
   devise_for :users
-  resources :listings
+  resources :listings do 
+    resources :orders, only: [:new, :create]
+  end
+
   get 'pages/about'
 
   get 'pages/contact'
   get 'seller' => "listings#seller"
+  get 'sales' => "order#sales"
+  get 'purchases' => "orders#purchases"
 
   root 'listings#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718052723) do
+ActiveRecord::Schema.define(version: 20150718054846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 20150718052723) do
     t.string   "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "listing_id"
+    t.integer  "buyer_id"
+    t.integer  "seller_id"
   end
 
   create_table "users", force: :cascade do |t|
